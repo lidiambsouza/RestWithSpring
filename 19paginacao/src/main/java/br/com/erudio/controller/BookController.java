0beb6be96dc5d6c5a24efdc06eeb6a9ea9ca29ed
ldsouza_avanade <ldsouza_avanade@timbrasil.com.br>
2021-11-23 19:30:20 -0300

    with query params

--- 19paginacao/src/main/java/br/com/erudio/controller/BookController.java
+++ 19paginacao/src/main/java/br/com/erudio/controller/BookController.java
@@ -3,11 +3,16 @@ package br.com.erudio.controller;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.data.vo.v1.BookVO;
@@ -34,10 +40,16 @@ public class BookController {
 //	@CrossOrigin(origins = "http://www.lidiateste.com.br")
 	@ApiOperation(value="find all books")
 	@GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
-	public List<BookVO> findAll() {
-		List<BookVO> books = service.findAll();
+	public ResponseEntity<PagedResources<BookVO>> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+	@RequestParam(value = "limit", defaultValue = "12") int limit,
+	@RequestParam(value = "direction", defaultValue = "asc") String direction,
+	PagedResourcesAssembler assembler) {
+		var sortDirection = "desc".equalsIgnoreCase(direction)? Direction.DESC: Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "title"));
+
+		Page<BookVO> books = service.findAll(pageable);
 		books.stream().forEach(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
-		return books;
+		return new ResponseEntity<>(assembler.toResource(books), HttpStatus.OK);
 	}
 	
 	@ApiOperation(value="find book by id")
--- 19paginacao/src/main/java/br/com/erudio/services/PersonServices.java
+++ 19paginacao/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,8 +1,8 @@
 package br.com.erudio.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,9 +24,14 @@ public class PersonServices {
 		return vo;
 	}
 	
-	public List<PersonVO> findAll() {
-		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
-	}	
+	public Page<PersonVO> findAll(Pageable pageable) {
+		var page = repository.findAll(pageable);
+		return page.map(this::convertToPersonVO);
+	}
+	
+	public PersonVO convertToPersonVO(Person entity) {
+		return DozerConverter.parseObject(entity, PersonVO.class);		
+	}
 	
 	public PersonVO findById(Long id) {
 
